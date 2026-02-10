232929a224f26e71bbb0ad4af3345a0113c97616
MAYURIDONGRE16 <mayuridongre190@gmail.com>
2026-02-10 11:19:42 +0530

    expressday2

---
+++ code/express/express_day2/url_rouding-node.js
@@ -0,0 +1,27 @@
+const http = require('http')
+const url = require('url')
+const app = http.createServer((req, res) => {
+
+    const urldata = url.parse(req.url, true)
+    console.log(urldata)
+
+    if(urldata.pathname==='/'){
+        res.write('home page')
+    }
+    else if(urldata.pathname === '/contact'){
+        res.write('contact page')
+    }
+    else if(urldata.pathname === '/about'){
+        res.write('about page')
+    }
+    else{
+        res.write('page not found')
+    }
+
+    res.end()
+})
+const PORT=3000
+const HOST='127.0.0.1'
+app.listen(PORT,HOST,()=>{
+    console.log(`server is running on http://${HOST}:${PORT} `)
+})
